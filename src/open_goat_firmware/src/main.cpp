6fedadfcf604d0f8df93ec58b27fb7a145d21874
amy <amylizzlep@gmail.com>
2026-08-17 23:24:29 +0100

    loathing

--- src/open_goat_firmware/src/main.cpp
+++ src/open_goat_firmware/src/main.cpp
@@ -5,10 +5,47 @@
 //
 // This file provides a minimal wiring example. Actual UART driver
 // instances / board-specific names must be supplied by the board layer.
-
-#include "service_ids.h"
+#include <cstdint>
+#include "../../open_mower_ros/services/service_ids.h"
 #include <globals.cpp>
 
+
+#include <ulog.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <mutex>
+#include <xbot-service/Io.hpp>
+#include "xbot-service/RemoteLogging.hpp"
+#include "xbot-service/portable/system.hpp"
+
+#ifdef ULOG_ENABLED
+void console_logger(ulog_level_t severity, char* msg, const void* arg) {
+	(void) arg;
+  	printf("[%s]: %s\n", ulog_level_name(severity), msg);
+}
+#endif
+
 int main() {
-    return 0;
+	xbot::service::system::initSystem();
+	xbot::service::startRemoteLogging();
+	#ifdef ULOG_ENABLED
+	ULOG_SUBSCRIBE(console_logger, ULOG_DEBUG_LEVEL);
+	#endif
+
+	emergency_service.start();
+	//diff_drive.start();
+	//mower_service.start();
+	//imu_service.start();
+	bms_service.start();
+	power_service.start();
+	gps_service.start();
+	//input_service.start();
+	high_level_service.start();
+
+	xbot::service::Io::start();
+
+	while (1) {
+		sleep(1);
+	}
 }
