58f3b60690cff693f7bae9b96882919f35959834
amy <amylizzlep@gmail.com>
2026-08-23 12:28:43 +0100

    how is that not a compile error

--- open_goat_firmware/src/services/emergency_service/emergency_service.cpp
+++ open_goat_firmware/src/services/emergency_service/emergency_service.cpp
@@ -37,33 +37,29 @@ uint32_t EmergencyService::OnLoop(uint32_t now_micros, uint32_t) {
 
 void EmergencyService::OnHighLevelEmergencyChanged(const uint16_t* new_value, uint32_t length) {
   (void)length;
-  {
-    last_high_level_emergency_message_ = xbot::service::system::getTimeMicros();
-  }
+  last_high_level_emergency_message_ = xbot::service::system::getTimeMicros();
   UpdateEmergency(new_value[0], new_value[1]);
 }
 
 uint32_t EmergencyService::CheckTimeouts(uint32_t now) {
   uint16_t reasons = 0;
   uint32_t block_time = UINT32_MAX;
-  {
-    if (TimeoutReached(now - last_high_level_emergency_message_, 1'000'000, block_time)) {
-      reasons |= EmergencyReason::TIMEOUT_HIGH_LEVEL;
-    }
+  
+  if (TimeoutReached(now - last_high_level_emergency_message_, 1'000'000, block_time)) {
+    reasons |= EmergencyReason::TIMEOUT_HIGH_LEVEL;
   }
+
   constexpr uint16_t potential_reasons = EmergencyReason::TIMEOUT_HIGH_LEVEL | EmergencyReason::TIMEOUT_INPUTS;
   UpdateEmergency(reasons, potential_reasons);
   return block_time;
 }
 
 void EmergencyService::UpdateEmergency(uint16_t add, uint16_t clear) {
-  {
-    uint16_t old_reason = reasons_;
-    reasons_ &= ~clear;
-    reasons_ |= add;
-    if (reasons_ == old_reason) {
-      return;
-    }
+  uint16_t old_reason = reasons_;
+  reasons_ &= ~clear;
+  reasons_ |= add;
+  if (reasons_ == old_reason) {
+    return;
   }
   SendStatus();
 }
